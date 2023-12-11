dcb126489f8c0fd1ec7b32edc3a2c1228cfe7a1c
Victor Neal <vicne17rb@gmail.com>
2023-12-11 16:09:43 -0500

    examne parcial resuelto

---
+++ AdminUsuarios/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Usuario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->correo_electrónico = $request->correo_electrónico;
+        $usuario->fecha_registro = $request->fecha_registro;
+        $usuario->save();
+        return "Usuario registrado Correctamente";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show( $id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->save();
+        return $usuario;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->correo_electrónico = $request->correo_electrónico;
+        $usuario->fecha_registro = $request->fecha_registro;
+        $usuario->save();
+        return "Usuario Modificado Correctamente";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( $id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->delete();
+        return "Usuario Eliminado Correctamente";
+    }
+}
